347794e6672e8ab9b341479c8e3c6340f1d69d35
Canleskis <91223490+Canleskis@users.noreply.github.com>
2024-10-08 18:30:09 +0200

    Use given Cargo.lock for building demos

--- scripts/deploy_showcase.sh
+++ scripts/deploy_showcase.sh
@@ -1,5 +1,7 @@
 particular_dir=${1:-"particular"}
 
+cp scripts/Cargo.lock $particular_dir/Cargo.lock
+
 for subfolder in $particular_dir/examples/*; do
     if [ -d $subfolder ]; then
         bash scripts/deploy_example.sh $subfolder content/demos $particular_dir
